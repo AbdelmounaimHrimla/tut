35ab389a4561764949d71ed1b0c008caf01935c1
AbdelmounaimHrimla <hrimla.abdelmounim@gmail.com>
2021-04-08 14:44:14 +0100

    first commit

---
+++ 1-intro.js
@@ -0,0 +1,9 @@
+const amount = 2;
+
+if (amount < 10) {
+    console.log('Small Number');
+} else {
+    console.log('Large Number');
+}
+
+console.log('This Is My First Node App');
---
+++ 4-names.js
@@ -0,0 +1,11 @@
+// Local
+
+const secret = 'SUPER SECRET';
+
+// Share
+
+const siwar = 'Siwar';
+const hrimla = 'Hrimla';
+
+
+module.exports = { siwar, hrimla }
